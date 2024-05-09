b6138909b3af0562d4e140cf0e5f83a26c2fd9a3
Tim <tim.smith2814@gmail.com>
2024-05-09 09:36:31 +0100

    Change over to db

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,19 @@
-export default function Home() {
+import { sql } from "@vercel/postgres";
+
+export default async function Home() {
+  const froguins = await sql`SELECT * FROM froguins`;
+
   return (
     <div>
       <h1>Froguins</h1>
-      <h3>Edwin</h3>
-      <p>Very good boy is our Edwin.</p>
-      <h3>Edwina</h3>
-      <p>Very good girl our Edwina is.</p>
-      <h3>Tarquin</h3>
-      <p>Tarquin is probably a baddy. We haven&apos;t figured it out yet</p>
-      <h3>Targuin</h3>
-      <p>
-        Targuin is probably going to defeat Tarquin one day. We haven&apos;t
-        figured it out yet
-      </p>
-      <h3>Pingu</h3>
-      <p>Pingu is a maniac. He taught kids how to be naughty.</p>
+      {froguins.rows.map((froguin) => {
+        return (
+          <div key={froguin.id}>
+            <h3>{froguin.name}</h3>
+            <p>{froguin.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
